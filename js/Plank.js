1914db347452ad66f3ae5bd4963840b96ea404d9
Shravya Gade <gk.shravya@gmail.com>
2021-04-27 12:09:52 -0500

    android

--- js/Plank.js
+++ js/Plank.js
@@ -6,7 +6,7 @@ const Plank = function(props){
     return(
     
             <Image source = {require('D:/RA/SeeSaw/js/res/plank_new.png')} 
-    style = {[plankStyle.plank ,{transform:[{rotate:(props.degree)+'deg'}]}]}  onLayout={event => {
+    style = {[plankStyle.plank]}  onLayout={event => {
         const layout = event.nativeEvent.layout;
         console.log('height:', layout.height);
         console.log('width:', layout.width);
